f92fb3fe37ca71547031106f51f746a5606bf496
arshakmm <arshak.manasyan.97@mail.ru>
2023-03-02 18:39:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ReactPaginate from 'react-paginate';
+import './pagiantion.css'
+import { Audio } from 'react-loader-spinner';
 
 function App() {
+
+  const [bookName, setBookName] = useState('')
+  const [books, setBooks] = useState([])
+  const [totalPages, setTotalPages] = useState(0)
+  const [loader, setLoader] = useState(false)
+
+  function searchBook() {
+    setLoader(true)
+    if (bookName !== '') {
+      fetch(`http://openlibrary.org/search.json?q=${bookName}&page=`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.docs.length) {
+            setBooks(data.docs)
+            setTotalPages(Math.ceil(data.numFound / 100))
+            setLoader(false)
+          }
+
+        })
+        .catch((erorr)=>{
+          console.log(erorr,'assdddsd')
+        })
+
+    }
+
+  }
+
+  function changePages(currentPage) {
+    fetch(`http://openlibrary.org/search.json?q=${bookName}&page=${currentPage}`)
+      .then(response => response.json())
+      .then(data => {
+        setBooks(data.docs);
+      })
+      .catch((erorr)=>{
+        console.log(erorr,'assdddsd')
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        onChange={(e) => setBookName(e.target.value)} />
+      <button onClick={searchBook}>Search</button>
+      <div>
+        {
+          loader ? <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+          />: (<div>{books.map(item => (
+            <div key={item._version_}>{item.title}</div>
+          ))}
+
+            {books.length ?
+              <ReactPaginate
+                activeClassName={'item active '}
+                breakClassName={'item break-me '}
+                containerClassName={'pagination'}
+                disabledClassName={'disabled-page'}
+                nextClassName={"item next "}
+                pageClassName={'item pagination-page '}
+                previousClassName={"item previous"}
+                breakLabel="..."
+                nextLabel="next >"
+
+                previousLabel="< prev"
+                renderOnZeroPageCount={null}
+                pageCount={totalPages}
+                onPageChange={(page) => changePages(page.selected + 1)}
+              /> : null}</div>)
+
+
+
+        }
+
+      </div>
+
+
     </div>
   );
 }
